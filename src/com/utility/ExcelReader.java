077c7a669f78e82a7046587febbc5c6da53b7002
nwarbade <nwarbade@gmail.com>
2023-06-07 19:38:23 +0530

    added more types

---
+++ src/com/utility/ExcelReader.java
@@ -0,0 +1,51 @@
+package com.utility;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelReader {
+	
+		
+	
+		public static void main(String[] args) throws IOException {
+		
+		
+		String path = "F:\\Github\\SeleniumRevision\\SampleData\\Sample1.xlsx";
+		
+		File file = new File(path);
+		
+		FileInputStream fis = new FileInputStream(file);
+		
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		
+		XSSFSheet sh1 = wb.getSheet("Sheet1");
+		
+	//	String datavalue = sh1.getRow(5).getCell(1).getStringCellValue();
+		
+		DataFormatter df = new DataFormatter();
+		
+		String datavalue = df.formatCellValue(sh1.getRow(7).getCell(0));
+		
+		System.out.println(datavalue);
+		
+	int lastindexposition = sh1.getLastRowNum();// returns the last index position from excel sheet containing data
+	
+	int rowcount = lastindexposition + 1;
+	
+	System.out.println(rowcount);
+	
+	int	columncount = sh1.getRow(0).getLastCellNum();// returns the actual column count from the excel sheet
+
+	System.out.println(columncount);
+	
+	
+
+}
+}
---
+++ src/com/utility/ExcelWriter.java
@@ -0,0 +1,35 @@
+package com.utility;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelWriter {
+	
+public static void main(String[] args) throws IOException {
+		
+		
+		String path = "F:\\Github\\SeleniumRevision\\SampleData\\Sample1.xlsx";
+		
+		File file = new File(path);
+		
+		FileInputStream fis = new FileInputStream(file);
+		
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		
+		XSSFSheet sh1 = wb.getSheet("Sheet1");
+		
+		File fout = new File(path);
+		
+		FileOutputStream fos = new FileOutputStream(fout);
+		
+		sh1.getRow(3).getCell(0).setCellValue("writingtest");
+		
+		 
+
+}
+}
